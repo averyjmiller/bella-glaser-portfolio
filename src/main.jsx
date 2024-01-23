14e275037aeebbfeaa14b2bc77ada2c5ef03167a
Avery Miller <averymiller2060@gmail.com>
2024-01-23 16:50:07 -0500

    added basic file structure for source code

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import App from './App.jsx'
 import './index.css'
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />
+      },
+      {
+        path: 'about',
+        element: <AboutPage />
+      },
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
