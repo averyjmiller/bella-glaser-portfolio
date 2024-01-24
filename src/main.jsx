6dea4843f67245981dad0e4a9dbe870729375b8d
Avery Miller <averymiller2060@gmail.com>
2024-01-24 17:16:56 -0500

    initialized the design and structure for the homepage

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App.jsx'
+
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,15 @@
+import placeholderimg from '../assets/images/placeholder-image.png';
+
 export default function HomePage() {
   return (
-    <div></div>
+    <div>
+      <img className="homePageImg" src={placeholderimg} alt="Placeholder Image"/>
+      <div className="d-flex justify-content-center" id="">
+        <div className="d-flex justify-content-evenly" id="homebtn-wrapper">
+          <button className="homebtns">Check me out!</button>
+          <button className="homebtns">Let's get in touch!</button>
+        </div>
+      </div>
+    </div>
   );
 }
