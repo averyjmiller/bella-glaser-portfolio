bc1da1d2c06642797a7b4f0285d7dcacc105f6df
Avery-lab <54604339+Avery-lab@users.noreply.github.com>
2024-01-28 19:02:11 -0500

    added custom styling to the homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,8 +7,8 @@ export default function HomePage() {
       <p id="quote">"dare to live in the <br/>unknown"</p>
       <div className="d-flex justify-content-center" id="">
         <div className="d-flex justify-content-between" id="homebtn-wrapper">
-          <button className="homebtns">Learn all about me!</button>
-          <button className="homebtns">Let's get in touch!</button>
+          <a href="./about" className="homebtns">all about me!</a>
+          <a href="./contact" className="homebtns">get in touch!</a>
         </div>
       </div>
     </div>
