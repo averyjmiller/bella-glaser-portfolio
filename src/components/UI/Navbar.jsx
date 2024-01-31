021f5dc5474a21d6e07ce3f7c1d4597f14e95b15
Avery Miller <averymiller2060@gmail.com>
2024-01-30 19:18:45 -0500

    implemented hamburger component in navbar for mobile screens (work-in-progress)

--- src/components/UI/Navbar.jsx
+++ src/components/UI/Navbar.jsx
@@ -1,18 +1,40 @@
+import Hamburger from './Hamburger';
+import { useState } from 'react';
+
 export default function Nav({ links }) {
+
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
+  const toggleHamburger = () => {
+    setHamburgerOpen(!hamburgerOpen)
+  };
+
+  let navStyle = {
+    background: '#234133b3',
+    padding: '5px',
+    borderRadius: '5px',
+    display: hamburgerOpen ? 'inline': 'none'
+  };  
+
   return (
     <nav>
       <header>
-        <div id="name">
-          Bella Marie Andrews
-        </div>
-        <div id="profession">
-          Actor/Artist
-        </div>
+        <a href="./" id="nav-header">
+          <div id="name">
+            Bella Marie Andrews
+          </div>
+          <div id="profession">
+            Actor/Artist
+          </div>
+        </a>
       </header>
       <div id="nav-items">
-        <div id="nav-pages">
+        <div id="nav-pages" style={navStyle}>
           {links.map((link) => link)}
         </div>
+      </div>
+      <div className="hamburger" onClick={toggleHamburger}>
+        <Hamburger />
       </div>
     </nav>
   );
