8423e33a23b0a8a78fec253d14eb0b193b480768
Avery Miller <averymiller2060@gmail.com>
2024-02-12 19:08:43 -0500

    made it so the footer isnt shown when the hamburger nav is open

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -5,15 +5,18 @@ const hamburgerHandler = async (e) => {
   let isOpen = slider.classList.contains('slide-in');
   let open = document.getElementById('open');
   let close = document.getElementById('close');
+  let footer = document.getElementById('homebtn-wrapper');
 
   if(isOpen) {
     slider.setAttribute('class', 'slide-out');
     open.setAttribute('style', 'display: inline');
     close.setAttribute('style', 'display: none');
+    footer.setAttribute('style', 'display: inline');
   } else {
     slider.setAttribute('class', 'slide-in');
     open.setAttribute('style', 'display: none');
     close.setAttribute('style', 'display: inline');
+    footer.setAttribute('style', 'display: none!important');
   }
 }
 
