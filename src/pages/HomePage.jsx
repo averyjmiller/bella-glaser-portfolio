1f908a65ec98b2fb6dfb6084d41ccfb64fa40bf5
Avery Miller <averymiller2060@gmail.com>
2024-02-13 21:01:51 -0500

    adjusted format and font for homepage and optimized for mobile screens

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ export default function HomePage() {
   return (
     <div>
       <img className="homePageImg" src={homepageHeadshot} alt="Placeholder Image"/>
-      <p id="quote">"dare to live in the <br/>unknown"</p>
+      <p id="quote">dare to live in the unknown.</p>
       <div className="d-flex justify-content-center">
         <div className="d-flex justify-content-between flex-wrap" id="homebtn-wrapper">
           <a href="./about" className="homebtns">all about me!</a>
