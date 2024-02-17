e26f20873a341c90b2df0b5bf66a21a278c438cd
Avery Miller <averymiller2060@gmail.com>
2024-02-17 15:41:57 -0500

    added logic to the nav so custom styling classes are added and removed when the nav bar is opened on mobile screens

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -5,17 +5,20 @@ const hamburgerHandler = async (e) => {
   let isOpen = slider.classList.contains('slide-in');
   let open = document.getElementById('open');
   let close = document.getElementById('close');
+  let navHeader = document.getElementById('nav-header');
   let footer = document.getElementById('homebtn-wrapper');
 
   if(isOpen) {
     slider.setAttribute('class', 'slide-out');
     open.setAttribute('style', 'display: inline');
     close.setAttribute('style', 'display: none');
+    navHeader.classList.remove('nav-open');
     footer.setAttribute('style', 'display: inline');
   } else {
     slider.setAttribute('class', 'slide-in');
     open.setAttribute('style', 'display: none');
     close.setAttribute('style', 'display: inline');
+    navHeader.classList.add('nav-open');
     footer.setAttribute('style', 'display: none!important');
   }
 }
