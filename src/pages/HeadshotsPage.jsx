e578269a968625835812faceb14abfac5f40a4c7
Avery Miller <averymiller2060@gmail.com>
2024-02-17 20:54:56 -0500

    implemented a headshot component that displays headshot photos on the headshot page

--- src/pages/HeadshotsPage.jsx
+++ src/pages/HeadshotsPage.jsx
@@ -1,8 +1,11 @@
+import Headshot from '../components/Headshot';
+import headshots from '../js/headshots';
 import './HeadshotsPage.css';
 
 export default function HeadshotsPage() {
   return (
     <>
+      <Headshot headshots={headshots} />
     </>
   );
 }
