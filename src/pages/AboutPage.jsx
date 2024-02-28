b7d43c76179b12ad2885837b1a600e2306a11173
Avery Miller <averymiller2060@gmail.com>
2024-02-27 19:50:47 -0500

    added instagram embed to about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,5 +1,14 @@
+import { InstagramEmbed } from 'react-social-media-embed';
+import './AboutPage.css'
+
 export default function AboutPage() {
   return (
-    <div></div>
+    <div className='container' id="about-wrapper">
+      <div className="row">
+        <div className="col">
+          <InstagramEmbed url="https://www.instagram.com/bellamarieandrews/" width={328} />
+        </div>
+      </div>
+    </div>
   );
 }
