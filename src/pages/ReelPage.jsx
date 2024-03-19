2dc952cb08b8b0b49dc53a2e1dcdb318483d934c
Avery Miller <averymiller2060@gmail.com>
2024-03-18 21:22:32 -0400

    implemented reel page

--- src/pages/ReelPage.jsx
+++ src/pages/ReelPage.jsx
@@ -1,6 +1,19 @@
+import reel from '../assets/bellamarieandrews.mp4';
+import './ReelPage.css';
+
 export default function ReelPage() {
   return (
-    <>
-    </>
+    <div className='container' id="reel-wrapper">
+      <div className="row">
+        <div className="col-12">
+          <h1>reel & media</h1>
+        </div>
+        <div className="col-12">
+          <video controls id="reel">
+            <source src={reel} type="video/mp4" />
+          </video>
+        </div>
+      </div>
+    </div>
   );
 }
