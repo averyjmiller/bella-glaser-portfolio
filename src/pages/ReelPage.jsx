f5ba12438409a3ef24a8a1ebdfe7c92ddf97e7a3
Avery Miller <averymiller2060@gmail.com>
2024-04-22 12:12:34 -0400

    added new video to reel page

--- src/pages/ReelPage.jsx
+++ src/pages/ReelPage.jsx
@@ -1,4 +1,7 @@
 import reel from '../assets/bellamarieandrews.mp4';
+import reel2 from '../assets/Bella-and-E.E.-Dream-Scene.mp4';
+import thumbnail1 from '../assets/images/bellamarieandrews-thumbnail.png';
+import thumbnail2 from '../assets/images/bellamarieandrews-thumbnail2.png';
 import './ReelPage.css';
 
 export default function ReelPage() {
@@ -6,11 +9,16 @@ export default function ReelPage() {
     <div className='container' id="reel-wrapper">
       <div className="row">
         <div className="col-12">
-          <h1>reel & media</h1>
+          <h1>reels & media</h1>
         </div>
-        <div className="col-12">
-          <video controls id="reel">
+        <div className="col-12 text-center pt-4">
+          <video poster={thumbnail1} controls id="reel">
             <source src={reel} type="video/mp4" />
+          </video>
+        </div>
+        <div className="col-12 text-center py-4">
+          <video poster={thumbnail2} controls id="reel">
+            <source src={reel2} type="video/mp4" />
           </video>
         </div>
       </div>
